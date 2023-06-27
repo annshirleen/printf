c02c7bce9ad96cdbf6959e983c55523a83abc3de
Allan Kinzi <112064203+Master034@users.noreply.github.com>
2023-06-27 23:34:57 +0300

    Update print_binary.c Add Separation

--- print_binary.c
+++ print_binary.c
@@ -1,26 +1,84 @@
 #include "main.h"
-/**
- * _print_binary - Print Binary
- * @num: Number
- */
-void _print_binary(unsigned int num)
-{
-	int bits[sizeof(unsigned int) * 8];
-	int index = 0;
-	int i;
 
-	if (num == 0)
+/**
+ * _print_integer - Print Numbers
+ * @num: Number
+ * @base:base
+ * @uppercase:uppercase
+ * @flags:flags
+ * @l_modifier:modifier
+ * @width:width
+ * Return: number of char printed
+ */
+void _print_integer(unsigned int num, int base, int uppercase, flags_t *flags, int l_modifier, int width)
+{
+
+	if (l_modifier == 'l')
+	{
+		long int l_num = (long int)num;
+		_print_formatted_integer(l_num, base, uppercase, flags,width);
+	}
+	else if (l_modifier == 'h')
+	{
+		short int h_num = (short int)num;
+		_print_formatted_integer(h_num, base, uppercase, flags,width);
+	}
+	else
+	{
+		_print_formatted_integer(num, base, uppercase, flags,width);
+	}
+}
+
+/**
+ * _print_formatted_integer - Print Numbers
+ * @num: Number
+ * @base:base
+ * @uppercase:uppercase
+ * @flags:flags
+ * @width:width
+ * Return: number of char printed
+ */
+void _print_formatted_integer(unsigned long int num, int base, int uppercase, flags_t *flags, int width)
+{
+	char buffer[sizeof(unsigned long int) * 8 + 1];
+	char digits[] = "0123456789abcdef";
+	int index = sizeof(buffer) - 1;
+	int n2 = num;
+	int num_digits = sizeof(buffer) - index - 1;
+	int padding = width - num_digits;
+	
+	if (flags->plus && n2 >= 0)
+	{
+		_putchar('+');
+	}
+	else if (flags->space && n2 >= 0)
+	{
+		_putchar(' ');
+	}
+	
+	if (n2 == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while (num > 0)
+	if (uppercase)
 	{
-		bits[index++] = num % 2;
-		num /= 2;
+		digits[10] = 'A';
 	}
-	for (i = index - 1; i >= 0; i--)
+	buffer[index] = '\0';
+	while (n2 > 0)
 	{
-		_putchar(bits[i] + '0');
+		buffer[--index] = digits[n2 % base];
+		n2 /= base;
+	}
+	while (padding > 0)
+	{
+		_putchar(' ');
+		padding--;
+	}
+	while (buffer[index] != '\0')
+	{
+		_putchar(buffer[index]);
+		index++;
 	}
 }
