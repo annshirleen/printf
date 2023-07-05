b961e07b9e3835c7c25eb25025d5519c193d3cf0
Shirleen Ann <annshirleen6@gmail.com>
2023-07-05 12:16:18 -0800

    _printfc

--- _printf.c
+++ _printf.c
@@ -5,152 +5,48 @@
  * @format:format
  * Return:integer
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-	flags_t flags = {0, 0, 0};
+	match_convert m[] = {
+		{"%s", _print_str}, {"%c", _print_chr},
+		{"%%", _print_cent},
+		{"%i", _print_int}, {"%d", _print_dec},
+		{"%b", _print_binary}, {"%u", _print_unsigned},
+		{"%o", _print_oct}, {"%x", _print_hex},
+		{"%X", _print_HEX}, {"%S", _print_excl_str},
+		{"%p", _print_ptr}, {"r", _print_str_rev},
+		{"%R", _print_rot13}
+	};
+
 	va_list args;
-	int count  = 0;
-	int l_modifier = 0;
-	int width = 0;
+	int i = 0, j, len = 0, match_found = 0;
 
 	va_start(args, format);
-	while (*format != '\0')
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	while ((format[i]) != '\0')
 	{
-		if (*format == '%')
+		match_found = 0;
+
+		for (j = 0; j < 14; j++)
 		{
-			format++;
-			while (*format == '+' || *format == ' ' || *format == '#')
-		       	{
-			       	switch (*format)
-				{
-				case '+':
-					flags.plus = 1;
-					break;
-				case ' ':
-					flags.space = 1;
-					break;
-				case '#':
-					flags.hash = 1;
-					break;
-				}
-				format++;
-			}
-			if (*format == 'l' || *format == 'h')
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				l_modifier = *format;
-				format++;
-			}
-			switch (*format)
-			{
-				 case 'b':
-					 {
-						 unsigned int num = va_arg(args, unsigned int);
-						 _print_binary(num);
-						 count += sizeof(unsigned int) * 8;
-					 }
-					 break;
-				case 'c':
-					{
-						int c = va_arg(args, int);
-
-						_putchar(c);
-						count++;
-					}
-					break;
-				case 's':
-					{
-						const char *s = va_arg(args, const char *);
-
-						while (*s != '\0')
-						{
-							_putchar(*s);
-							s++;
-							count++;
-						}
-					}
-					break;
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				case 'u':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						_print_integer(num, 10, 0,&flags,l_modifier, width);
-						count += sizeof(unsigned int) * 8;
-					}
-					break;
-				case 'o':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						_print_integer(num, 8, 0,&flags,l_modifier, width);
-						count += sizeof(unsigned int) * 3;
-					}
-					break;
-				case 'd':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						_print_integer(num, 16, 0,&flags,l_modifier, width);
-						count += sizeof(unsigned int) * 2;
-					}
-					break;
-				case 'i':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						_print_integer(num, 16, 1,&flags,l_modifier, width);
-						count += sizeof(unsigned int) * 2;
-					}
-					break;
-				case 'x':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						_print_integer(num, 16, 0,&flags,l_modifier, width);
-						count += sizeof(unsigned int) * 2;
-					}
-					break;
-				case 'X':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						_print_integer(num, 16, 1,&flags,l_modifier, width);
-						count += sizeof(unsigned int) * 2; 
-					}
-					break;
-				case 'S':
-					{
-						const char *str = va_arg(args, const char *);
-						_print_string(str);
-					}
-					break;
-				case 'p':
-					{
-						void *ptr = va_arg(args, void *);
-						_print_pointer(ptr, &flags,l_modifier,width);
-						count += sizeof(void *) * 2;
-					}
-					break;
-
+				len += m[j].f(args);
+				i += 2;
+				match_found = 1;
+				break;
 			}
 		}
-		else
+		if (!match_found)
 		{
-			_putchar(*format);
-			count++;
+			_putchar(format[i]);
+			len++;
+			i++;
 		}
-		format++;
 	}
 	va_end(args);
-	return (count);
-
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	return (len);
 }
