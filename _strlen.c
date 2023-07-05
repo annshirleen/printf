028a4067410dbc40aecbe16ac01399dc486506c3
annshirleen <annshirleen6@gmail.com>
2023-07-05 13:40:34 -0700

    print_f

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @s: string
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[c] != 0)
+		c++;
+	return (count);
+
+}
---
+++ _strlenc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlenc - function that calculates length of constants for s
+ * @s: string
+ * Return: count
+ */
+int _strlenc(const char *s)
+{
+	int count = 0;
+
+	while (s[c] != 0)
+		c++;
+	return (count);
+}
